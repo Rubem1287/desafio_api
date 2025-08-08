6e1c48e81ab49b15f33d40d6ae9e4f9a1066fd22
Rubem <40901612+Rubem1287@users.noreply.github.com>
2025-08-08 01:45:01 -0300

    Delete Controller.js

--- Controller.js
+++
@@ -1,9 +0,0 @@
-class Controller {
-  async casdatroLivro(requisicao, resposta) {}
-
-  async atualizarCadastroLivro(requisicao, resposta) {}
-
-  async deletarCadastroLivro(requisicao, resposta) {}
-}
-
-module.exports = () => new Controller();
